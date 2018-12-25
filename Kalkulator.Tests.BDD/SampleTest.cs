f6492f9dd451be4153118e5c7663630273262357
TomekM <o2.tomek@tlen.pl>
2018-12-25 22:12:22 +0100

    install xunit and add sample test

---
+++ Kalkulator.Tests.BDD/SampleTest.cs
@@ -0,0 +1,39 @@
+﻿using LightBDD.Framework;
+using LightBDD.Framework.Scenarios.Basic;
+using LightBDD.XUnit2;
+
+namespace Kalkulator.Tests.BDD
+{
+    [FeatureDescription("Description")]
+    [Label("LIFO")]
+    public partial class SampleTest
+    {
+        [Scenario]
+        [Label("Empty")]
+        [ScenarioCategory("Category")]
+        public void Empty_stack()
+        {
+            Runner.RunScenario(
+                given_an_empty_stack,
+                then_it_has_no_elements,
+                then_throws_an_exception_when_calling_pop,
+                then_it_throws_an_exception_when_calling_peek
+                );
+        }
+
+        [Scenario]
+        [Label("Not empty")]
+        public void Non_empty_stack()
+        {
+            Runner.RunScenario(
+                given_a_non_empty_stack,
+                when_calling_peek,
+                then_it_returns_the_top_element,
+                then_it_does_not_remove_the_top_element,
+                when_calling_pop,
+                then_it_returns_the_top_element,
+                then_it_removes_the_top_element
+                );
+        }
+    }
+}
---
+++ Kalkulator.Tests.BDD/SampleTestFixture.cs
@@ -0,0 +1,76 @@
+﻿using LightBDD.XUnit2;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+[assembly: LightBddScope]
+
+namespace Kalkulator.Tests.BDD
+{
+    [FeatureFixture]
+    public partial class SampleTest : FeatureFixture
+    {
+        public SampleTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        private Stack<int> stack;
+        private int result;
+
+        // Empty
+
+        private void given_an_empty_stack()
+        {
+            stack = new Stack<int>();
+        }
+
+        private void then_it_has_no_elements()
+        {
+            Assert.Empty(stack);
+        }
+
+        private void then_throws_an_exception_when_calling_pop()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+
+        private void then_it_throws_an_exception_when_calling_peek()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Peek());
+        }
+
+
+        // Not empty
+
+        private void given_a_non_empty_stack()
+        {
+            stack = new Stack<int>(new[] { 1, 2, 3 });
+        }
+
+        private void when_calling_peek()
+        {
+            result = stack.Peek();
+        }
+
+        private void then_it_returns_the_top_element()
+        {
+            Assert.Equal(result, 3);
+        }
+
+        private void then_it_does_not_remove_the_top_element()
+        {
+            Assert.True(stack.Contains(3));
+        }
+
+        private void when_calling_pop()
+        {
+            result = stack.Pop();
+        }
+
+        private void then_it_removes_the_top_element()
+        {
+            Assert.False(stack.Contains(result));
+        }
+    }
+}
